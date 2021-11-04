17cd0a9eb8dbd24d9393b3734da9a4c1ce70e756
lemon-cake123 <faizan.athaurrahman@gmail.com>
2021-11-04 12:17:07 +0700

    updated readme to have instalation instructions

--- organizer_run.bat
+++ organizer_run.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python39\pythonw.exe" "C:\Users\%USERNAME%\downloads_sorter\organizer.py"
+"C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python39\pythonw.exe" "C:\Users\%USERNAME%\Organizer\organizer.py"
